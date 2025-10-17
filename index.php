71c3f51c7fee0e4c3aabf7a920fed521aabd61d1
edukir099 <meireleseduardo15@gmail.com>
2025-10-16 22:17:15 -0300

    Atualização da página index

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css_js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script src="../css_js/bootstrap/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../css_js/css/estilo_form_cadastro_aluno.css">
+    <link href="css_js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="css_js/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css_js/css/estilo_form_cadastro_aluno.css">
 
     <title>Cadastro do aluno</title>
 </head>
@@ -30,55 +30,68 @@
         <fieldset style="width: 650px;height: 580px;">
 
             <form method="POST" action="salvar_cadastro_aluno_bd.php" style="width: 650px; padding: 20px;"
-                enctype="multipart/form-data">
-                <div class="dados">
-                    <h5>DADOS</h5>
-                    <small>NOME COMPLETO</small> <br>
-                    <input type="text" name="txt_nome" placeholder="Nome completo" autofocus="" required size="80px">
-                    <br>
-                    <div style="position: relative; top: -140px; margin-left: 440px;">
-                        <small>DATA DE NASCIMENTO</small> <br>
-                        <input type="date" name="txt_data_nascimento">
+				enctype="multipart/form-data">
+				<div class="dados">
+					<h5>DADOS</h5>
+                    <div class="col-md-6">
+						<label class="form-label">NOME COMPLETO</label> <br>
+					<input type="text" name="txt_nome" placeholder="Nome completo" autofocus="" required size="80px" class="form-control"> <br>
                     </div>
-                    <small>CPF</small> <br>
-                    <input type="text" name="txt_cpf" placeholder="000.000.000-00"> <br>
-                </div>
-
-                <div class="endereco">
-                    <h5>ENDEREÇO</h5>
-                    <small>LOGRADOURO</small> <br>
-                    <input type="text" name="txt_endereco" placeholder="Rua / Av: Tal, 000" size="80px" style="width: 580px"> <br>
-                    <small>CIDADE</small> <br>
-                    <input type="text" name="txt_cidade" placeholder="0000">
-                    <small>CEP</small> <br>
-                    <input type="text" name="txt_cep" placeholder="0000">
-                    <small>UF</small> <br>
-                    <input type="text" name="txt_uf" placeholder="0000">
-                    <div style="position: relative; top: -90px; margin-left: 440px;">
-                        <small>COMPLEMENTO</small> <br>
-                        <input type="text" name="txt_complemento" placeholder="0000">
+                    <div>
+						<label class="form-label">DATA DE NASCIMENTO</label> <br>
+					<input type="date" name="txt_data_nascimento" class="form-control">
                     </div>
-
-                    <div style="position: relative; top: -140px; margin-left: 440px;">
-                        <small>NUMERO</small> <br>
-                        <input type="number" name="txt_numero"> <br>
+                    <div>
+						<label class="form-label">CPF</label> <br>
+					<input type="text" name="txt_cpf" placeholder="00000-000" class="form-control"> <br>
                     </div>
-                </div>
+				</div>
 
-                <h5>CONTATO</h5>
-                <small>Telefone</small> <br>
-                <input type="text" name="txt_telefone" placeholder="Telefone Ex:(00) 0000-0000">
+				<div class="endereco">
+					<h5>ENDEREÇO</h5>
+                    <div class="col-md-6">
+						<label class="form-label">LOGRADOURO</label> <br>
+					<input type="text" name="txt_endereco" placeholder="Rua / Av: Tal, 000" size="80px" class="form-control"> <br>
+                    </div>
+                    <div>
+						<label class="form-label">CIDADE</label> <br>
+					<input type="text" name="txt_cidade" placeholder="0000" class="form-control">
+                    </div>
+                    <div>
+						<label class="form-label">CEP</label> <br>
+					<input type="text" name="txt_cep" placeholder="0000" class="form-control">
+                    </div>
+                    <div class="col-md-4">
+						<label class="form-label">UF</label> <br>
+					<input type="text" name="txt_uf" placeholder="0000" class="form-control">
+                    </div>
+                    <div>
+						<label class="form-label">COMPLEMENTO</label> <br>
+					<input type="text" name="txt_complemento" placeholder="0000" class="form-control">
+                    </div>
+                    <div>
+						<label class="form-label">NUMERO</label> <br>
+					<input type="number" name="txt_numero" class="form-control"> <br>
+                    </div>
+				</div>
 
-                <div style="position: relative; top: -50px; margin-left: 440px;">
-                    <small>Celular</small> <br>
-                    <input type="text" name="txt_celular" placeholder="Celular Ex:(00) 00000-0000"> <br>
-                </div>
-                <small>E-mail</small> <br>
-                <input type="text" name="txt_email" placeholder="emailteste@gmail.com" size="80px"> <br>
-
-        </fieldset>
-        </form>
-    </div>
+					<h5>CONTATO</h5>
+                    <div>
+						<label class="form-label">Telefone</label> <br>
+					<input type="text" name="txt_telefone" placeholder="Telefone Ex:(00) 0000-0000" class="form-control">
+                    </div>
+                    <div>
+						<label class="form-label">Celular</label> <br>
+					<input type="text" name="txt_celular" placeholder="Celular Ex:(00) 00000-0000" class="form-control"> <br>
+                    </div>
+                    <div>
+						<label class="form-label">E-mail</label> <br>
+					<input type="text" name="txt_email" placeholder="emailteste@gmail.com" size="80px" class="form-control"> <br>
+                    </div>
+				</div>
+			</form>
+		</fieldset>
+	</div>
 </body>
 
 </html>
